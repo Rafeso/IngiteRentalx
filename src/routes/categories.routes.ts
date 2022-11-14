048e92ca0c415b7558d67691b46d22e2105ad3da
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-11-14 18:04:53 -0300

    Commit: Inserindo ID com uuid.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,16 +1,19 @@
 import { request, Router } from "express";
-
+import { v4 as uuidv4 } from "uuid";
 const categoriesRoutes = Router();
 
 const categories = [];
 
-categoriesRoutes.post("/categories", (req, res) => {
+categoriesRoutes.post("/", (req, res) => {
 	const { name, description } = req.body;
 
-	categories.push({
+	const category = {
+		id: uuidv4(),
 		name,
 		description,
-	});
+	};
+
+	categories.push(category);
 
 	return res.status(201).send();
 });
