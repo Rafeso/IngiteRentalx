29ebcc9e068aee2cd581faba824a43bd6d93ddda
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-11-14 17:58:15 -0300

    Commit: Criando categoria.

---
+++ src/routes/categories.routes.ts
@@ -0,0 +1,18 @@
+import { request, Router } from "express";
+
+const categoriesRoutes = Router();
+
+const categories = [];
+
+categoriesRoutes.post("/categories", (req, res) => {
+	const { name, description } = req.body;
+
+	categories.push({
+		name,
+		description,
+	});
+
+	return res.status(201).send();
+});
+
+export { categoriesRoutes };
