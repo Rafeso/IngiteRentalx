0f3b90e1f70a9cca444115e121c35f9befefdf7a
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-11-15 09:23:16 -0300

    Commit: Validando o cadastro de categoria.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -8,6 +8,12 @@ const categoriesRepository = new CategoriesRepository();
 categoriesRoutes.post("/", (req, res) => {
 	const { name, description } = req.body;
 
+	const categoryAlreadyExists = categoriesRepository.findByName(name);
+
+	if (categoryAlreadyExists) {
+		return res.status(400).json({ error: "Category Already Exists!" });
+	}
+
 	categoriesRepository.create({ name, description });
 
 	return res.status(201).send();
@@ -16,7 +22,7 @@ categoriesRoutes.post("/", (req, res) => {
 categoriesRoutes.get("/", (req, res) => {
 	const listAll = categoriesRepository.list();
 
-  return res.json(listAll)
+	return res.json(listAll);
 });
 
 export { categoriesRoutes };
