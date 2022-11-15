8a881f8ad1487ad046489d7df2c80f76b2bc838f
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-11-15 09:13:34 -0300

    Commit: Criando repositório de categoria.

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,26 +1,16 @@
 import { Router } from "express";
-import { v4 as uuidv4 } from "uuid";
 
-import { Category } from "../model/category";
+import { CategoriesRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoriesRepository = new CategoriesRepository();
 
 categoriesRoutes.post("/", (req, res) => {
 	const { name, description } = req.body;
 
-	const category = new Category();
+	categoriesRepository.create({ name, description });
 
-	Object.assign(category, {
-		name,
-		description,
-		createdAt: new Date(),
-	});
-
-	categories.push(category);
-
-	return res.status(201).json({ category });
+	return res.status(201).send();
 });
 
 export { categoriesRoutes };
