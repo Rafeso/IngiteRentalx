244323ff641dda7e959f72b18a9cfe10b91d2c8f
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-11-17 16:55:55 -0300

    Commit: Utilizando o Princípio de Liskov

--- src/repositories/CategoriesRepository.ts
+++ src/repositories/CategoriesRepository.ts
@@ -1,11 +1,10 @@
 import { Category } from "../model/category";
+import {
+	ICategoriesRepository,
+	ICreateCategoryDTO,
+} from "./ICategoriesRepository";
 
-interface ICreateCategoryDTO {
-	name: string;
-	description: string;
-}
-
-class CategoriesRepository {
+class CategoriesRepository implements ICategoriesRepository {
 	private categories: Category[] = [];
 
 	constructor() {
@@ -35,3 +34,4 @@ class CategoriesRepository {
 }
 
 export { CategoriesRepository };
+
