00c818315ddfaff262b8793e93b553c3ec5da154
Rafael Feitosa <rafaefeitosa@gmail.com>
2022-11-18 12:32:20 -0300

    Commit: Conhecendo Singleton Pattern.

--- src/modules/cars/useCases/listCategories/Index.ts
+++ src/modules/cars/useCases/listCategories/Index.ts
@@ -2,8 +2,10 @@ import { CategoriesRepository } from "../../repositories/CategoriesRepository";
 import { ListCategoriesUseCase } from "./ListCategoriesUseCase";
 import { ListCategoriesController } from "./ListCategoriesController";
 
-const categoriesRepository = new CategoriesRepository();
+const categoriesRepository = CategoriesRepository.getInstance();
+
 const listCategoriesUseCase = new ListCategoriesUseCase(categoriesRepository);
+
 const listCategoriesController = new ListCategoriesController(
 	listCategoriesUseCase
 );
